25a54b98a399c9226d48761676990b9da92f37af
pmaddaloni@gmail.com <pmaddaloni@gmail.com@19600413-3b0a-8eab-5a19-ea0cc7dec3e8>
2012-02-23 20:03:29 +0000

    more commands supported

--- trunk/SOE/src/203-easybot.hs
+++ trunk/SOE/src/203-easybot.hs
@@ -1,45 +1,7 @@
 import Array
+import Data.List.Split
 
 main = sim initState
-
-data Command = Lt
-             | Rt
-             | Fd Int
-
-data Program = Single Command
-             | Sequence Program Program
-
-sim::RobotState->IO ()
-sim s = do cmdStr <- getLine
-           let cmd = (parse cmdStr)
-           let s' = run_c cmd s
-           printState s'
-           sim s' 
-
-run_c::Command->RobotState->RobotState
-run_c (Fd x) s = do  (s {position = newPos})
-                     where newPos = (movePos (position s) (facing s))
-run_c (Lt) s = do (s {facing = left (facing s)})
-run_c (Rt) s = do (s {facing = right (facing s)})
-
-parse::String->Command
-parse str = case str of
-              "left" -> (Lt)
-              "fwd" -> (Fd 10)
-
-movePos :: Position -> Direction -> Position
-movePos (x,y) d
-  = case d of
-      North -> (x,y+1)
-      South -> (x,y-1)
-      East  -> (x+1,y)
-      West  -> (x-1,y)
-
-printState :: RobotState -> IO ()
-printState s
-  = do putStrLn "Ending Robot State:"
-       putStrLn ("  Position:  " ++ show (position s))
-       putStrLn ("  Facing:    " ++ show (facing s))
 
 data RobotState 
   = RobotState 
@@ -49,7 +11,8 @@ data RobotState
      deriving Show
 
 initState :: RobotState
-initState = RobotState { position = (0,0)
+initState = RobotState 
+				{ position = (0,0)
                 , facing   = North
                 }
 
@@ -62,3 +25,56 @@ right,left :: Direction -> Direction
 
 right d = toEnum (succ (fromEnum d) `mod` 4)
 left  d = toEnum (pred (fromEnum d) `mod` 4)
+
+data Command = Lt
+             | Rt
+             | Fd Int
+			 | Bk Int
+			 | No_Op
+
+data Program = Single Command
+             | Sequence Program Program
+
+sim::RobotState-> IO ()
+sim s = do cmdStr <- getLine
+           let cmd = parse (splitOn " " cmdStr)
+           let s' = run_c cmd s
+           printState s'
+           sim s' 
+
+run_c :: Command -> RobotState -> RobotState
+run_c (Fd x) s = do  (s {position = newPos})
+                     where newPos = (movePos (position s) x (facing s))
+run_c (Bk x) s = do  (s {position = newPos})
+                     where newPos = (movePos (position s) x (facing s))
+run_c (Lt) s = do (s {facing = left (facing s)})
+run_c (Rt) s = do (s {facing = right (facing s)})
+run_c (No_Op) s = s
+
+parse :: [String] -> Command
+parse (c:[]) = case c of
+				"left" -> (Lt)
+				"right" -> (Rt)
+				"exit" -> error "\nGoodbye!"
+				_ -> (No_Op)
+parse (cmd:val:tail) = case cmd of
+				"fwd" -> (Fd (read $ val :: Int))
+				"backward" -> (Bk (read $ val :: Int))
+				_ -> (No_Op)
+				
+
+movePos :: Position -> Int -> Direction -> Position
+movePos (x,y) v d
+  = case d of
+      North -> (x,y+v)
+      South -> (x,y-v)
+      East  -> (x+v,y)
+      West  -> (x-v,y)
+
+printState :: RobotState -> IO ()
+printState s
+  = do putStrLn "Current Robot State:"
+       putStrLn ("  Position:  " ++ show (position s))
+       putStrLn ("  Facing:    " ++ show (facing s))
+
+
